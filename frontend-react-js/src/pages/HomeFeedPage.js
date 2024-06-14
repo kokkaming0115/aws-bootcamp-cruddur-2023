c7368649f602bb1e33bf892f2e6c589bdc39572a
martin <ka_ming_martin_kok@hotmail.com>
2024-06-14 12:04:28 +0800

    Fix the project and run locally

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -21,16 +21,21 @@ export default function HomeFeedPage() {
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+      
       const res = await fetch(backend_url, {
         method: "GET"
       });
+      
       let resJson = await res.json();
       if (res.status === 200) {
+        
         setActivities(resJson)
       } else {
+        
         console.log(res)
       }
     } catch (err) {
+      
       console.log(err);
     }
   };
